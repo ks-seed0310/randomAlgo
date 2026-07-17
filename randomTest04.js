47c08293a30111f0dcd526a5b2072158f83cc0b8
KS_ks562406 <230470204+ks-seed0310@users.noreply.github.com>
2026-07-17 21:49:59 +0900

    Update randomTest04.js

--- randomTest04.js
+++ randomTest04.js
@@ -6,7 +6,9 @@ class random1{
   constructor(){
     this.#v1=BigInt(Date.now()>>>0)
     this.#v2=this.#stir32b(Date.now())
-    this.#v3=BigInt(crypto.getRandomValues(new Uint32Array(1))[0])
+    const c=crypto.getRandomValues(new Uint32Array(4))
+    const ch=[...c].map(x=>x.toString(16).padStart(8,"0"))
+    this.#v3=BigInt("0x"+c.join(""))
     this.#v4=this.#stir32b(this.#v3)
   }
   #cs(v,s){
@@ -20,62 +22,65 @@ class random1{
     const c5=0xa8cf0ce8n;
     let re=BigInt(seed);
     function t(v){
-      return v&0xFFFFFFFFn
+      return v&0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFn
     }
     function cs(V,s){
       let v=t(V)
-      return (v<<s)|(v>>(32n-s))
+      return (v<<s)|(v>>(128n-s))
     }
-    for (let i=0n;i<24n;i+=1n){
+    for (let i=0n;i<48n;i+=1n){
       re=t(re*c1)
       re=t(cs(re,i^8n)*19n+c1)
       re=t(re+c2)
       re=t(re*c2)
       re=t(re^c3)
+      re=t(re*c4*c5*127n)
       re=t((16n*re+123987n+c1+c3)^c2)
       re=t(3n*(re*(i+1n)^c2)+c1)
       re=cs(re,13n)
     }
+    re^=re>>48n
     re^=1511207693n
-    re^=re>>16n
+    re^=re>>32n
     re=re*0x85ebca6bn
-    re^=re>>13n
+    re^=re>>24n
     re=re*0xc2b2ae35n
     re^=re>>16n
     return t(re)
   }
   random1(){
     function t(v){
-      return v&0xFFFFFFFFn
+      return v&0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFn
     }
     function cs(V,s){
       let v=t(V)
-      return t((v<<s)|(v>>(32n-s)))
+      return t((v<<s)|(v>>(128n-s)))
     }
     let s1=this.#v1
     let s2=this.#v2
     let s3=this.#v3
     let s4=this.#v4
-    this.#v1=t(s2+1986875077n)
+    this.#v1=t(s2+0x6444856E3F4EA63An^(s1>>17n))
     s2^=s2<<23n
     this.#v2=t(s1^s2^s3^s4^(s1>>6n)^(s2>>12n)^(s3>>18n)^(s4>>24n))
-    this.#v3=t((s1<<5n)^(BigInt(this.#v2)>>7n)^cs(s3^s4,13n)^(174440041n*s4))
+    this.#v3=t((s1<<10n)^(BigInt(this.#v2)>>10n)^cs(s3^s4,13n)^(00000000174440041n*s4))
     const _v1=this.#v1
     const _v2=this.#v2
     const _v3=this.#v3
     const _v4_s=cs((cs(_v1*s4,16n)^cs(_v2*_v3,12n)^cs(_v1*_v3,8n)^cs(_v2*s4,7n))*(((_v1+_v2+_v3+s4)*(_v1^_v2^_v3^s4))>>5n),11n)
-    this.#v4=t((_v4_s^(_v4_s>>7n)^(_v4_s>>13n)^(_v4_s>>19n)^(256n^(_v4_s&0x00000293n))))
+    this.#v4=t((_v4_s^(_v4_s>>14n)^(_v4_s>>26n)^(_v4_s>>38n)^(256n^(_v4_s&0x0000000000157293n))))
     const _v4=this.#v4
-    let g32=t(cs(((_v2+_v3)^_v4^_v1),17n)^(cs(_v2,17n)>>24n)^((_v1>>8n)^(_v2>>12n)^(_v3>>16n)^(_v4>>20n)>>5n)^t(cs(((_v1+_v2)*_v3)+_v4,23n)))
-    g32^=(g32>>15n)
-    g32^=((g32<<7n)&0x9d2c5680n)
-    g32^=(t(_v1+_v2+_v3+_v4)>>16n)
-    g32=cs(g32,11n)
-    return Number(t(g32))
+    let g64=t(cs(((_v2+_v3)^_v4^_v1),34n)^(cs(_v2,17n)>>24n)^((_v1>>16n)^(_v2>>24n)^(_v3>>32n)^(_v4>>40n)>>10n)^t(cs(((_v1+_v2)*_v3)+_v4,46n)))
+    g64^=(g64>>15n)
+    g64^=((g64<<7n)&0x9d2c5680n)
+    g64^=(t(_v1+_v2+_v3+_v4)>>8n)
+    g64=cs(g64,22n)
+    let g32=g64>>96n
+    return Number(g32&0xFFFFFFFFn)
   }
 }
 const r = new random1();
-const CHUNK_SIZE = 65536; 
+const CHUNK_SIZE = 32768; 
 function runLoop() {
     for (let i = 0; i < CHUNK_SIZE; i++) {
         console.log(r.random1());
